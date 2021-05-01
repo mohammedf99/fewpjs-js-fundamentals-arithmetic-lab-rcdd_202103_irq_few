c0a1e0382bf32a608b5dce93ebf94f5ccb89567c
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-01 02:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 // Write your code here
 
 
-const multiply = (num1, num2) => {
-  
-  return num1 * num2;
-  
-}
+const multiply = (num1, num2) => num1 * num2;
 
 const random = Math.floor((Math.random() * 10) + 1);
 
-function mod(num3, num4) {
-  const mod = num3 % num4;
-}
+const mode = (num3, num4) => num3 % num4;
 
 function max(num) {
   const max = Math.max(num);
