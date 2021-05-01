cf5b1dadc470d59ff3b9fe1aeba510b565e9e91a
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-01 02:41:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Write your code here
 
 
+function multiply(num1, num2) {
+  const multiply = num1 * num2;
+}
+
 const random = Math.floor((Math.random() * 10) + 1);
 
-const mod = 5 % 2;
+function mod(num3, num4) {
+  const mod = num3 % num4;
+}
+
