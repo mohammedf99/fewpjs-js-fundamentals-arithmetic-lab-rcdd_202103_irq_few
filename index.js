d32490ab2c2cc050877f896aa1b134bf5f6b1735
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-01 02:53:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const multiply = num1 * num2;
 
 const random = Math.floor((Math.random() * 10) + 1);
 
-const mode = num3 % num4;
+const mod = num3 % num4;
 
 const max = Math.max(1, 3, 5, 7, 20);
 
