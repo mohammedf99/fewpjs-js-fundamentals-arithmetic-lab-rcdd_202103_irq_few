81fd3a012022f9098c978cf1ad4452d9b13d4feb
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-01 02:53:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Write your code here
 
-const num1 = 2, num2 = 31, num;
+const num1 = 2, num2 = 31, num3 = 18, num4 = 7;
 
-const multiply = num1 * num2
+const multiply = num1 * num2;
 
 const random = Math.floor((Math.random() * 10) + 1);
 
-const mode = (num3, num4) => num3 % num4;
+const mode = num3 % num4;
 
-function max(num) {
-  const max = Math.max(num);
-}
+const max = Math.max(1, 3, 5, 7, 20);
+
